ceacaea71f6871f28eee542f88d97094f008ee7b
Eclipse91 <58339346+Eclipse91@users.noreply.github.com>
2024-02-12 19:47:39 +0100

    Update main.py
    I added some comments

--- main.py
+++ main.py
@@ -4,17 +4,21 @@ from weather_app import WeatherApp
 from config_reader import ConfigReader
 
 def main():
-    # Logger
+    # Configure and initialize the logger
     logging.basicConfig(filename='weather_app_logger.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     logging.info('Program started')
+
+    # Read configuration parameters from config.ini
     parameters = ConfigReader()
     parameters.read_config_file()
 
     if parameters.parameters:
+        # Create the Tkinter root window and start the WeatherApp
         root = tk.Tk()
         app = WeatherApp(root, parameters)
         root.mainloop()
     else:
+        # Display a message if there are configuration issues
         print('Configuration issues detected in config.ini. See weather_app_logger.log for details.')
     
     logging.info('Program Ended')
